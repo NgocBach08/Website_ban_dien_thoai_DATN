c0ae839a550b38c777a203225257e08877fbe850
NgocBach08 <habinhk2@gmail.com>
2023-10-10 21:40:04 +0700

    update

--- src/main/java/com/example/testhtml/constant/ConstansErrorCode.java
+++ src/main/java/com/example/testhtml/constant/ConstansErrorCode.java
@@ -9,6 +9,11 @@ public interface ConstansErrorCode {
 
     String CATEGORY_ID = "WP-LG-002";
 
+    String CATEGORY_NAME_SAME = "WP-LG-003";
+
+    String LOGIN_ACCOUNT_BLOCKED = "WP-LG-04";
+
+    String LOGIN_EMAIL_NOT_EXITS = "WP-LG-005";
 
     String STAFF_EMAIL = "WP-LG-006";
 
