9c4289cf92e42640fcb53f560e842a680eb6f549
hieu230201 <hieuop130316@gmail.com>
2023-10-10 21:31:13 +0700

    udpate

--- src/main/java/com/example/testhtml/service/impl/ChipServiceImpl.java
+++ src/main/java/com/example/testhtml/service/impl/ChipServiceImpl.java
@@ -19,39 +19,8 @@ public class ChipServiceImpl implements IChipService {
     private final ChipRepo repo;
 
 
-    @Override
-    public List<ChipRespone> findAll() {
-        List<ChipRespone> list = new ArrayList<>();
-        List<ChipEntity> entities = repo.findByDeleteFlagIsFalse();
-        for (ChipEntity e: entities
-        ) {
-            list.add(new ChipRespone(String.valueOf(e.getId()), e.getName()));
-        }
-        return list;
-    }
 
-    @Override
-    public ChipRespone findByCate(String id) {
-        ChipEntity chip = repo.getById(Long.valueOf(id));
-        return new ChipRespone(String.valueOf(chip.getId()), chip.getName());
-    }
 
-    @Override
-    public String createChip(ChipRequestDto request) {
-        ChipEntity chip = new ChipEntity();
-        chip.setName(request.getName());
-        repo.save(chip);
-        return "ok";
-    }
-
-    @Override
-    public String updateChip(ChipRequestDto request) {
-        ChipEntity chip = new ChipEntity();
-        chip.setName(request.getName());
-        chip.setId(Long.valueOf(request.getId()));
-        repo.save(chip);
-        return "ok";
-    }
 
     @Override
     public String deleteChip(Long id) {
