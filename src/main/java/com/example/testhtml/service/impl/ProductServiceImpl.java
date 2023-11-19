5f14bb6028e7511695ca9e46075b89ac7ef550e1
hieu230201 <hieuop130316@gmail.com>
2023-11-19 20:02:39 +0700

    sua FE

--- src/main/java/com/example/testhtml/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/testhtml/service/impl/ProductServiceImpl.java
@@ -73,6 +73,10 @@ public class ProductServiceImpl implements IProductService {
             return String.valueOf(new WorldPhoneExp(ConstansErrorCode.ROM_NOT_EXIST).getErrorMessage().getVn());
         }
         ProductEntity entity = mapToRequest(requestProduct);
+        java.util.Date now = new java.util.Date();
+
+        entity.setCreateDate(new Timestamp(now.getTime()));
+
         entity.setStatus(StatusProduct.NGUNG_KINH_DOANH.getStatus());
         entity = productRepo.save(entity);
         if(!attributeProductService.saveAttribute(requestProduct.getAttributeRequestAdd(), entity.getId()).equals("ok")){
