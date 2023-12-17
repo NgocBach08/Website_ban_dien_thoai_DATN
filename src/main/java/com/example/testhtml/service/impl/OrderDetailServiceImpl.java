c0a8bf1933eecc5f5960513fac0d97dbeaf94e7e
hieu230201 <hieuop130316@gmail.com>
2023-12-18 00:15:54 +0700

    sua FE

--- src/main/java/com/example/testhtml/service/impl/OrderDetailServiceImpl.java
+++ src/main/java/com/example/testhtml/service/impl/OrderDetailServiceImpl.java
@@ -1,19 +1,16 @@
 package com.example.testhtml.service.impl;
 
 import com.example.testhtml.common.StatusImei;
-import com.example.testhtml.entity.ImeiEntity;
-import com.example.testhtml.entity.OrdersDetailEntity;
-import com.example.testhtml.entity.OrdersEntity;
-import com.example.testhtml.entity.ProductPropertyEntity;
-import com.example.testhtml.repo.ImeiRepo;
-import com.example.testhtml.repo.OrdersDetailRepo;
-import com.example.testhtml.repo.OrdersRepo;
-import com.example.testhtml.repo.PropertyProductRepo;
+import com.example.testhtml.common.StatusOrder;
+import com.example.testhtml.dto.respone.order_detail.OrderDetailRespone;
+import com.example.testhtml.entity.*;
+import com.example.testhtml.repo.*;
 import com.example.testhtml.service.IOrderDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Description:
@@ -32,18 +29,43 @@ public class OrderDetailServiceImpl implements IOrderDetailService {
     private  final PropertyProductRepo propertyProductRepo;
 
     private final ImeiRepo imeiRepo;
+
+    private final VoucherRepo voucherRepo;
     @Override
     public String deleteDetail(Long id, String codeOrder) {
         OrdersDetailEntity detailEntity = detailRepo.findByDeleteFlagIsFalseAndId(id);
         OrdersEntity entity = detailEntity.getOrdersEntity();
-        if (entity != null && entity.getTotalMoney() != null) {
-            entity.setTotalMoney(entity.getTotalMoney() - (detailEntity.getQuantity() * detailEntity.getPrice()));
+        List<OrdersDetailEntity> ordersDetailEntities = detailRepo.findByDeleteFlagIsFalseAndOrdersEntity(entity);
+        Long tongTien = 0L;
+        for (OrdersDetailEntity detail: ordersDetailEntities
+        ) {
+            tongTien+=(detail.getPrice()* detail.getQuantity());
+        }
+        if (tongTien - (detailEntity.getQuantity() * detailEntity.getPrice()) < 0) {
+            tongTien = 0L;
+        } else {
+            tongTien = tongTien - (detailEntity.getQuantity() * detailEntity.getPrice());
+        }
+        if (entity != null && entity.getTotalMoney() != null ) {
+            Optional<VoucherEntity> voucherEntity = voucherRepo.findById(String.valueOf(entity.getVoucherID()));
+            if (voucherEntity.isPresent() && tongTien > 0L) {
+                if(voucherEntity != null){
+                    if(voucherEntity.get().getTypeDiscount().equals("đ")){
+                        tongTien = tongTien - voucherEntity.get().getDiscount();
+                    }else {
+                        tongTien = tongTien - (tongTien / 100 * voucherEntity.get().getDiscount());
+                    }
+                }
+            }
+            entity.setTotalMoney(tongTien);
             ordersRepo.save(entity);
         }
         detailEntity.setDeleteFlag(true);
         ProductPropertyEntity entityPro = propertyProductRepo.findById(detailEntity.getIdPropertyProduct()).get();
         List<ImeiEntity> imeiEntityList = imeiRepo.findByOrderDetailIdAndPropertyProductId(detailEntity.getId(), entityPro.getId());
-        entityPro.setQuantity(detailEntity.getQuantity() + entityPro.getQuantity());
+        if (entity.getStatus().equals(String.valueOf(StatusOrder.CHO_GIAO_HANG.getIndex()))) {
+            entityPro.setQuantity(detailEntity.getQuantity() + entityPro.getQuantity());
+        }
         imeiEntityList.forEach(x -> x.setStatus(StatusImei.CHUA_BAN.getValue()));
         imeiRepo.saveAll(imeiEntityList);
         propertyProductRepo.save(entityPro);
