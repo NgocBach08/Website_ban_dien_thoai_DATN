2db38e91e3647882b8b2f32706c59a790441f2b8
hoangth <ohbigmom@gmail.com>
2023-12-19 21:56:02 +0700

    updateImeiAPI

--- src/main/java/com/example/testhtml/config/MvcConfig.java
+++ src/main/java/com/example/testhtml/config/MvcConfig.java
@@ -1,15 +1,15 @@
-    //package com.example.testhtml.config;
-    //
-    //import org.springframework.context.annotation.Configuration;
-    //import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-    //import sdfasdfaorg.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-    //
-    //@Configurationádfasf
-    //public class MvcConfig implements WebMvcConfigurer {
-    //
-    //    @Override
-    //    public void addViewControllers(ViewControllerRegistry registry) {
-    //        System.out.println("12312312");
-    //        registry.addViewController("/not-found").setViewName("error-404");
-    //    }
-    //}
+//package com.example.testhtml.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import sdfasdfaorg.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configurationádfasf
+//public class MvcConfig implements WebMvcConfigurer {
+//
+//@Override
+//public void addViewControllers(ViewControllerRegistry registry) {
+//System.out.println("12312312");
+//registry.addViewController("/not-found").setViewName("error-404");
+//}
+//}
